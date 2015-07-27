802c5ea5ddb2ebd99bedd59567243395ab604d0e
Tim <tim@nudjed.com>
2015-07-27 13:56:59 +0100

    first commit

---
+++ booking-form.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Booking Form
+Plugin URI: https://github.com/Skehel/TODO
+Description: A plugin for creating and managing booking forms for a wordpress site.
+Version: 0.0.1
+Author: Tim Skehel
+Author URI: http://timskehel.com
+License: TODO
+*/
