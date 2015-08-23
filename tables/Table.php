12bdb40d0e7ac8c39718904393a63d0c431edf06
Tim <tim@nudjed.com>
2015-08-23 01:17:58 +0100

    Table users $wpdb functionality making it simpler

--- tables/Table.php
+++ tables/Table.php
@@ -36,7 +36,7 @@ abstract class Table {
      */
     function get($id)
     {
-        $select = "SELECT * from $this->name WHERE id= ".$this->getPlaceholder($id);
+        $select = "SELECT * from $this->name WHERE id=%s";
         return $this->wpdb->get_row(
             $this->wpdb->prepare($select, $id),
             ARRAY_A);
@@ -50,28 +50,7 @@ abstract class Table {
      */
     function insert($data)
     {
-        $insert = "INSERT INTO $this->name (";
-        $values = "VALUES (";
-
-        $i = 1;
-        foreach($data as $column => $value)
-        {
-            $insert .= $column;
-            $values .= $this->getPlaceholder($value);
-
-            if($i < sizeOf($data))
-            {
-                $insert .= ', ';
-                $values .= ', ';
-            }
-            $i++;
-        }
-
-        $insert .= ') '.$values.')';
-
-        return $this->wpdb->query(
-            $this->wpdb->prepare($insert, $data),
-            ARRAY_A);
+        return $this->wpdb->insert($this->name, $data);
     }
 
     /**
@@ -82,51 +61,16 @@ abstract class Table {
      */
     function update($id, $data)
     {
-        $update = "UPDATE $this->name SET ";
-
-        $i = 1;
-        foreach ($data as $column => $value)
-        {
-            $update .= $column.'=';
-            $update .= $this->getPlaceholder($value);
-
-            if($i < sizeOf($data))
-            {
-                $update .= ', ';
-            }
-            $i++;
-        }
-        $update .= " WHERE id=".$this->getPlaceholder($id);
-        $data['id'] = $id;
-
-        return $this->wpdb->query($this->wpdb->prepare($update, $data));
+        return $this->wpdb->update($this->name, $data, ['id' => $id]);
     }
 
     /**
      * deletes the record with the specified id
      * @param $id
-     * @return mixed
+     * @return boolean false on failure
      */
     function delete($id)
     {
-        $delete = "DELETE FROM $this->name WHERE id= ".$this->getPlaceholder($id);
-        return $this->wpdb->query( $this->wpdb->prepare($delete, $id) );
-    }
-
-    /**
-     * Checks a values type and returns a sprintf placeholder for $wpdb->prepare()
-     * @param $value
-     * @return string
-     */
-    private function getPlaceholder($value)
-    {
-        $type = gettype($value);
-
-        if($type == 'int' || $type == 'double')
-        {
-            return '%d';
-        }
-
-        return '%s';
+        return $this->wpdb->delete($this->name, ['id' => $id]);
     }
 }
