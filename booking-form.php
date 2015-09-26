8f1d734be3d23179ebab929a38a55db70022a3df
Tim <tim@nudjed.com>
2015-09-26 11:23:43 +0100

    booking-form documentation has been updated with liscense and repository

--- booking-form.php
+++ booking-form.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: Booking Form
-Plugin URI: https://github.com/Skehel/TODO
+Plugin URI: https://github.com/Skehel/wordpress-booking-form
 Description: A plugin for creating and managing booking forms for a wordpress site.
 Version: 0.0.1
 Author: Tim Skehel
 Author URI: http://timskehel.com
-License: TODO
+License: MIT
 */
 
 require(__DIR__.'/vendor/autoload.php');
